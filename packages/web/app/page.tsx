6e11420ee0bc89c7fc2cb6115269447a05dceea0
Pierre Obermaier <pierre.obermaier@googlemail.com>
2025-08-08 18:16:15 +0200

    R Removing hume and related code

--- packages/web/app/page.tsx
+++ packages/web/app/page.tsx
@@ -1,16 +1,9 @@
-import { getHumeAccessToken } from "@/utils/getHumeAccessToken";
 import Chat from "@/components/Chat";
 
 export default async function Page() {
-  const accessToken = await getHumeAccessToken();
-
-  if (!accessToken) {
-    throw new Error("Unable to get access token");
-  }
-
   return (
     <div className={"grow flex flex-col"}>
-      <Chat accessToken={accessToken} />
+      <Chat />
     </div>
   );
 }
