9b79c3b6604e34010e703824b91c15b30516e235
Pierre Obermaier <pierre.obermaier@googlemail.com>
2025-08-08 13:37:36 +0200

    R Upgrading to newest next version 15.4.6

--- packages/web/components/logos/Hume.tsx
+++ packages/web/components/logos/Hume.tsx
@@ -1,4 +1,4 @@
-import type { FC, SVGAttributes } from "react";
+import type { SVGAttributes } from "react";
 import { useId } from "react";
 
 export type HumeLogoProps = SVGAttributes<SVGSVGElement>;
