724a9dc4d705b63539f1b35ec8f5f0cd629aca1b
Pierre Obermaier <pierre.obermaier@googlemail.com>
2025-08-21 16:01:06 +0800

    F Introducing question mode in UI

--- packages/web/context/ChatContext.tsx
+++ packages/web/context/ChatContext.tsx
@@ -1,11 +1,15 @@
 import { createContext, Dispatch, SetStateAction } from "react";
 
 export const ChatContext = createContext<{
+  mode: "memory" | "question" | undefined;
+  setMode: Dispatch<SetStateAction<"memory" | "question" | undefined>>;
   isRecording: boolean;
   setIsRecording: Dispatch<SetStateAction<boolean>>;
   transcriptions: string[];
   setTranscriptions: Dispatch<SetStateAction<string[]>>;
 }>({
+  mode: undefined,
+  setMode: () => {},
   isRecording: false,
   setIsRecording: () => {},
   transcriptions: [],
