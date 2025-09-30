cc81d70cff76c25b60cc17854be848e918b614d3
Pierre Obermaier <pierre.obermaier@googlemail.com>
2025-09-30 20:57:16 +0200

    R Fixing layout using grid layout

--- packages/web/app/page.tsx
+++ packages/web/app/page.tsx
@@ -3,9 +3,9 @@ import Header from "@/components/ui/Header";
 
 export default function Home() {
   return (
-    <div className="font-sans h-screen flex flex-col">
+    <div className="font-sans h-screen grid grid-flow-row grid-rows-[62px_1fr]">
       <Header />
-      <main className="flex-1 p-12">
+      <main className="p-6 min-h-0 overflow-hidden">
         <Chat />
       </main>
     </div>
--- packages/web/components/ui/Header.tsx
+++ packages/web/components/ui/Header.tsx
@@ -4,8 +4,8 @@ import { FC } from "react";
 
 const Header: FC = () => {
   return (
-    <header className="flex p-4 px-12 shadow-md">
-      <Link href="/" className="flex items-center">
+    <header className="p-4 px-12 shadow-md">
+      <Link href="/" className="items-center">
         <Image src="/logo.png" alt="Momento Logo" width={122} height={30} />
       </Link>
     </header>
