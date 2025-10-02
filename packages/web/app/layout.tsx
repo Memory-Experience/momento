b1e6083355720b223247d15ec2dd3f39308ae257
Pierre Obermaier <pierre.obermaier@googlemail.com>
2025-10-02 14:03:06 +0200

    F Introducing initial dark/light mode switch

--- packages/web/app/layout.tsx
+++ packages/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +24,22 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // NOTE: ThemeProvider requires suppressHydrationWarning to suppress hydration warning
+  // as the theme differs between server and client rendering due to systems preference.
+  // See the note at the bottom of https://github.com/pacocoursey/next-themes?tab=readme-ov-file#with-app
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
